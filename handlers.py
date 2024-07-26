015b006ebf2c6d3bf19af4e5924c6d9e0b9e60f4
Ubuntu <ubuntu@ip-172-31-35-173.ec2.internal>
2024-07-26 22:31:01 +0000

    update handlers

--- handlers.py
+++ handlers.py
@@ -1,164 +1,78 @@
-import uuid, redis, json, math
+import uuid
+import redis
+import json
+import math
 
 redis = redis.StrictRedis()
 
-def distance(x1, x2):
-    return math.sqrt(sum([(x1[i]-x2[i])**2 for i in range(len(x1))])) 
+def calculate_distance(point1, point2):
+    return math.sqrt(sum((p1 - p2) ** 2 for p1, p2 in zip(point1, point2)))
 
+def is_bid_matching(bid, robot_data):
+    if bid.get('service') not in robot_data.get('services', []):
+        return False
+    
+    bid_location = (bid.get('lat', 0), bid.get('lon', 0))
+    robot_location = (robot_data.get('lat', 0), robot_data.get('lon', 0))
+    
+    return calculate_distance(bid_location, robot_location) < robot_data.get("max_distance", 1)
 
-def matches(bid, robot_data):
+def grab_job(robot_data):
+    matched_bids = []
+    for bid_id, bid_json in redis.hscan_iter("REDHASH_ALL_LIVE_BIDS"):
+        bid = json.loads(bid_json)
+        if is_bid_matching(bid, robot_data):
+            matched_bids.append((bid.get('price', 0), bid_id, bid))
 
-    #print(bid, robot_data)
-    # Check that the robot can do the job
-    res = bid.get('service') in robot_data.get('services', [])
-    if res == False:
-        return res
-    print('here1')
-    # Check that the robot timeframe and the bid timeframe overlap  ## time in utc timestamp
-    #res = bid.get('end_time', 0) < robot_data.get('end_time', 1)
-    #if res == False:
-    #    return res
-    #print('here1')
-    # Check that the travel distance is acceptable # later factor in distance
-    res = distance((robot_data.get('lat', 0), robot_data.get('lon', 0)), (bid.get('lat', 0), bid.get('lon', 0))) < robot_data.get("max_distance",1)
-    #print(distance((robot_data.get('lat', 0), robot_data.get('lon', 0)), (bid.get('lat', 0), bid.get('lon', 0))))
+    if not matched_bids:
+        return None, 204
 
-    print(robot_data.get("max_distance"))
-    print('here!')
-    if res == False:
-        return res
-    return True
+    best_match = max(matched_bids, key=lambda x: x[0])
+    _, bid_id, job = best_match
 
-def grab_job(data):
-    # get all matching bids
-    matched = []
-    for bid_id, bid in redis.hscan_iter("REDHASH_ALL_LIVE_BIDS"):
-        bid = json.loads(bid)
-        if matches(bid, data):
-            matched.append((bid.get('price', 0), bid_id, bid))
+    job_id = str(uuid.uuid4())
+    new_job = {
+        'job_id': job_id,
+        'bid_id': bid_id,
+        'status': 'won',
+        'job_request': robot_data,
+        'bid_params': job,
+    }
 
-    # select the highest
-    if matched:
-        _m = sorted(matched, key=lambda x: -x[0])
-        job = _m[0][2]  # Access the bid data
+    redis.hset("REDHASH_ALL_WINS", bid_id, json.dumps(new_job))
+    redis.hdel("REDHASH_ALL_LIVE_BIDS", bid_id)
 
-        # Create a new job ID
-        job_id = str(uuid.uuid4())
+    return new_job, 200
 
-        # Create the job object
-        new_job = {
-            'job_id': job_id,
-            'bid_id': _m[0][1],
-            'status': 'won',
-            'job_request': data,  # Include job request parameters
-            'bid_params': job,  # Include bid parameters
-        }
-
-        # Move the bid to the wins hash
-        redis.hset("REDHASH_ALL_WINS", _m[0][1], json.dumps(new_job)) 
-
-        # Remove the bid from the live bids hash
-        redis.hdel("REDHASH_ALL_LIVE_BIDS", _m[0][1])
-
-        return new_job, 200
-    else:
-        return None, 204  # No matching bids
-
-def sufficient_funds(data):
-    # check that account has sufficient balance
-    account_id = data.get("account_id",-1)
+def has_sufficient_funds(data):
+    account_id = data.get("account_id")
     bid_price = data.get("bid_price", 0)
-    account = redis.hget("REDHASH_ACCOUNTS", account_id)
-    if account and bid_price:
-        account = json.loads(account)
-        all_outstanding = []
-        for bid_id, b in redis.hscan_iter("REDHASH_ALL_LIVE_BIDS"):
-            b = json.loads(b)
-            if b.get("bidder_account_id") == account_id:
-                all_outstanding.append(b)
-        if account.get("balance",0) - sum([b.get("bid_price",0) for b in all_outstanding]) + data.get("bid_price",0) > 0:
-            response, status = {"message" : "sufficient funds"}, 200
-    response, status = {"message" : "insufficient funds or malformed request"}, 400
-    return True 
-
-def certify(data):
-    # check that the request is certified 
-    return True
+    
+    account_json = redis.hget("REDHASH_ACCOUNTS", account_id)
+    if not account_json or not bid_price:
+        return False
+    
+    account = json.loads(account_json)
+    outstanding_bids = [
+        json.loads(b) for _, b in redis.hscan_iter("REDHASH_ALL_LIVE_BIDS")
+        if json.loads(b).get("bidder_account_id") == account_id
+    ]
+    
+    total_outstanding = sum(b.get("bid_price", 0) for b in outstanding_bids)
+    return account.get("balance", 0) - total_outstanding + bid_price > 0
 
 def submit_bid(data):
-    # check bid certificate
-    certificate = certify(data)
-    if sufficient_funds(data) and certificate:
-       # make bid_id
-        bid = data.get('bid', {})
-        bid_id = str(uuid.uuid4())
-        redis.hset("REDHASH_ALL_LIVE_BIDS", bid_id, json.dumps(bid)) 
-        return bid_id, 200 
-    return bid_id, 403
+    if not has_sufficient_funds(data):
+        return "", 403
 
+    bid = data.get('bid', {})
+    bid_id = str(uuid.uuid4())
+    redis.hset("REDHASH_ALL_LIVE_BIDS", bid_id, json.dumps(bid))
+    return bid_id, 200
 
-def nearby_activity(data):
+def get_nearby_activity():
     all_live_bids = redis.hgetall("REDHASH_ALL_LIVE_BIDS")
-
-    return json.dumps(all_live_bids), 200 
-
-
+    return json.dumps(all_live_bids), 200
 
 if __name__ == "__main__":
-    # Test 1: Submit a bid and grab the job
-    print("Test 1: Submit Bid and Grab Job")
-
-    # Setup: Define bid and robot data
-    bid_data = {
-        'service': 'delivery',
-        'lat': 40.7128,
-        'lon': -74.0060,
-        'price': 100,
-        'end_time': 1680000000,  # Timestamp for future date
-    }
-    robot_data = {
-        'services': ['delivery'],
-        'lat': 40.7128,
-        'lon': -74.0060,
-        'max_distance': 10,
-        'end_time': 1680000000,
-    }
-
-    # Submit the bid
-    submit_bid({'bid': bid_data})
-
-    # Grab the job
-    job = grab_job(robot_data)
-
-    # Check for successful job creation
-    assert job is not None
-    assert job['job_id'] is not None
-    assert job['bid_id'] is not None
-    assert job['status'] == 'won'
-
-    # Cleanup: Remove the bid from wins hash
-    redis.hdel("REDHASH_ALL_WINS", job['bid_id'])
-
-    # Test 2: Submit a bid that doesn't match
-    print("Test 2: Submit Bid that Doesn't Match")
-
-    # Setup: Define bid data that doesn't match the robot
-    bid_data = {
-        'service': 'cleaning',
-        'lat': 40.7128,
-        'lon': -74.0060,
-        'price': 100,
-        'end_time': 2680000000,
-    }
-
-    # Submit the bid
-    submit_bid({'bid': bid_data})
-
-    # Grab the job
-    job = grab_job(robot_data)
-
-    # Check that no job was created
-    assert job is None
-
-    # Cleanup: Remove the bid from live bids hash (since it didn't match)
-    redis.hdel("REDHASH_ALL_LIVE_BIDS", job['bid_id'])
+    print("This is the main module. Run tests.py to execute the tests.")
