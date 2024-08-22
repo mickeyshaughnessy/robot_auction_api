cd64a2a75263912a6f08f755f623b7df41d951e9
Ubuntu <ubuntu@ip-172-31-35-173.ec2.internal>
2024-08-22 23:37:53 +0000

    add utils

---
+++ utils.py
@@ -0,0 +1,12 @@
+import redis
+import hashlib
+
+def setup_redis(host, port, db):
+    return redis.Redis(host=host, port=port, db=db)
+
+def cleanup_redis(r):
+    for key in r.scan_iter("*"):
+        r.delete(key)
+
+def generate_signature(password, job_id):
+    return hashlib.sha256(f"{password}{job_id}".encode()).hexdigest()
