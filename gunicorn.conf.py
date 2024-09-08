2f8fdfa78457c54170c8e3309d11ef14e427ee88
Michael Shaughnessy <mickeyshaughnessy>
2024-09-08 17:49:28 -0600

    bind

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,11 +1,9 @@
 import multiprocessing
 from config import API_PORT
 
-bind = [f"0.0.0.0:{API_PORT}"]
+bind = f"0.0.0.0:{API_PORT}"
 workers = multiprocessing.cpu_count() * 2 + 1
-
 raw_env = ["FLASK_ENV=production"]
-
 worker_class = "gevent"
 timeout = 300
 keepalive = 5
